5ca49266e8671908b11fd716eb28e007ed50af58
Destiny-Kay <destinykevogo@gmail.com>
2024-06-11 14:53:43 +0300

    Fix QA errors

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -3,9 +3,9 @@ import { uploadPhoto, createUser } from './utils';
 export default async function asyncUploadUser() {
   let res = {};
   try {
-    const uploadRes = await uploadPhoto();
-    const userRes = await createUser();
-    res = { uploadRes, userRes };
+    const photo = await uploadPhoto();
+    const user = await createUser();
+    res = { photo, user };
   } catch (error) {
     res = { photo: null, user: null };
   }
