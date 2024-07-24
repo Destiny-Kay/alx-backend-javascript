8ecbc7dbe99101937226de6cc49578ba77a3d0fd
Destiny-Kay <destinykevogo@gmail.com>
2024-07-24 04:58:58 +0300

    Initial

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,9 @@
+/**
+ *DIsplays a message on STFOUT
+ @param {string} arg THe message to be displayed
+ */
+function displayMessage(arg) {
+  console.log(arg);
+}
+
+module.exports = displayMessage;
